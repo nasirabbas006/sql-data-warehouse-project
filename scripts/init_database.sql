bb7d33686637e045fc5be11bda056f8916e21135
nasirabbas006 <138598219+nasirabbas006@users.noreply.github.com>
2025-03-10 21:25:51 +0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+
+--Create Database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+Go
+
+USE DataWarehouse;
+Go
+--creating schemas
+CREATE SCHEMA bronze;
+Go
+
+CREATE SCHEMA silver;
+Go
+
+CREATE SCHEMA gold;
+Go
